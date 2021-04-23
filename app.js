1f6933833a44c890a15b4f067230ea1276eb4f9e
Habib Vadillo <habibvar@gmail.com>
2021-04-23 14:48:51 +0200

    nothing

--- app.js
+++ app.js
@@ -26,20 +26,22 @@ const capitalized = (string) =>
 app.locals.title = `${capitalized(projectName)} created with Ironlauncher`;
 
 // Set up session
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-app.use(session ({
-  secret: process.env.SESSION_KEY,
-  saveUninitialized: false, 
-  resave: false, 
-  cookie: {
-    maxAge: 24 * 60 * 60 * 1000
-  },
-  store: MongoStore.create({
-    mongoUrl:  process.env.MONGODB_URI || "mongodb://localhost/m2_proj_DanHab",
-    ttl:  24 * 60 * 60 
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+app.use(
+  session({
+    secret: process.env.SESSION_KEY,
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI || "mongodb://localhost/m2_proj_DanHab",
+      ttl: 24 * 60 * 60,
+    }),
   })
-}));
+);
 
 // 👇 Start handling routes here
 const index = require("./routes/index");
@@ -50,7 +52,6 @@ app.use("/", location);
 
 const auth = require("./routes/auth.routes");
 app.use("/", auth);
-
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
--- routes/location.routes.js
+++ routes/location.routes.js
@@ -1,11 +1,7 @@
-const router = require("express").Router()
+const router = require("express").Router();
 
+router.get("/locations", (req, res, next) => {
+  res.render("index");
+});
 
-
-
-router.get('/locations', (req, res, next) {
-  
-})
-
-
-module.exports = router
+module.exports = router;
