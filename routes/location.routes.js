899ebed84efd3f1b8a794cf158dcf60ddd575d05
Daniel Traci <tracidaniel.td@gmail.com>
2021-04-26 18:38:56 +0300

    update and delete ops

--- routes/location.routes.js
+++ routes/location.routes.js
@@ -25,4 +25,36 @@ router.get("/locations", (req, res, next) => {
     .catch((err) => {});
 });
 
+
+// EDIT LOCATION
+router.get("/user/locations/:id/edit", (req, res, next) => {
+  const {_id} = req.params
+  Location.findById(_id)
+  .then((result) => {
+    res.render('user/update-location.hbs', {result})
+  }).catch((err) => {
+    console.log(err)
+  });
+})
+
+router.post("/user/locations/:id/edit", (req, res, next) => {
+  const {_id} = req.params
+  const {name, location} = req.body
+  Location.findByIdAndUpdate(_id, {name, location})
+  .then((result) => {
+    res.redirect('/user/locations')
+  }).catch((err) => {
+    console.log(err)
+  });
+})
+
+// DELETE LOCATION
+router.post('/user/locations/:id/delete', (req, res, next) => {
+  const {id}= req.params
+  Location.findByIdAndDelete(id)
+  .then(()=>res.redirect('/user/locations'))
+  .catch((err)=>console.log(err))
+});
+
+
 module.exports = router;
