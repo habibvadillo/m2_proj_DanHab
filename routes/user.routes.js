950e4e57d65bbb706f8128dc3a33e8144315fc0e
Habib Vadillo <habibvar@gmail.com>
2021-04-26 12:49:43 +0200

    search functionality

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,45 +1,40 @@
 const router = require("express").Router();
 
-const Location = require('../models/Location.model')
+const Location = require("../models/Location.model");
 
 //business profile
 
-router.get('/user/locations/create', (req, res, next) => {
-  res.render('locations/locations-create.hbs')
-})
+router.get("/user/locations/create", (req, res, next) => {
+  res.render("locations/locations-create.hbs");
+});
 
 router.post("/user/locations/create", (req, res, next) => {
+  const { name, location } = req.body;
 
-  const {name, location} = req.body
-  
-  const {_id} = req.session.userInfo
-  console.log(name, location, _id)
-  Location.create({name, location, owner: _id})
-  .then((result) => {
-
-    res.redirect("user/locations")
-  }).catch((err) => {
-    console.log(err)
-  });
-})
+  const { _id } = req.session.userInfo;
+  console.log(name, location, _id);
+  Location.create({ name, location, owner: _id })
+    .then((result) => {
+      res.redirect("/user/locations");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 router.get("/user/locations", (req, res, next) => {
-  const {_id} = req.session.userInfo
-  console.log(_id)
-  Location.find({owner: _id})
-  
-  .then((locations) => {
-    res.render('user/user-locations', {locations})
-  }).catch((err) => {
-    console.log(err)
-  });
-})
+  const { _id } = req.session.userInfo;
+  console.log(_id);
+  Location.find({ owner: _id })
 
-
-
-
-
+    .then((locations) => {
+      res.render("user/user-locations", { locations });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 //personal profile
 
-module.exports = router
+module.exports = router;
--- seeds/location.seed.js
+++ seeds/location.seed.js
@@ -1,15 +1,21 @@
-/*let Location = require("../models/Location.model")
+let Location = require("../models/Location.model");
+const mongoose = require("mongoose");
 
-require('../db')
+require("../db");
 
-const myLocations = {
-  {name: "St Moritz", location: "Zurich", owner = ""}
+const myLocations = [
+  { name: "Everest", location: "Malaysia" },
+  { name: "Himalaya", location: "Russia" },
+  { name: "Fuji Town", location: "Moldova" },
+  { name: "Red Snow", location: "Russia" },
+  { name: "Great Alps", location: "Russia" },
+];
 
-}
-
-const
-
-
-
-Location.create()
-*/
+Location.create(myLocations)
+  .then(() => {
+    console.log("locations created", myLocations);
+    mongoose.connection.close();
+  })
+  .catch((err) => {
+    mongoose.connection.close();
+  });
